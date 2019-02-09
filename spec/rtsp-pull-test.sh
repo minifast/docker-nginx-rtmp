3ea413ea0652c099f23224a5030520cc223f541e
Doc Ritezel <doc@ministryofvelocity.com>
2019-02-09 15:48:20 -0800

    Download http-based audio sources locally

--- spec/rtsp-pull-test.sh
+++ spec/rtsp-pull-test.sh
@@ -17,19 +17,13 @@ it_double_encodes_urls() {
 }
 
 it_configures_ffmpeg_for_25fps_rtsp_to_rtmp() {
-  expected="-fflags nobuffer -flags low_delay -strict experimental -rtsp_transport tcp -r 25 -i http://example.com -c:v copy -c:a aac -ar 44100 -f flv rtmp://localhost/rtsp-25/http%253A%252F%252Fexample.com"
-  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com" 25)
+  expected="-use_wallclock_as_timestamps 1 -probesize 32 -fflags nobuffer -flags low_delay -rtsp_transport tcp -r 25 -i http://example.com/source -movflags +faststart -shortest -fflags +genpts -c:v copy -c:a aac -ac 2 -ar 44100 -ab 128k -f flv rtmp://localhost/rtsp-25/destination"
+  actual=$(rtsp_to_rtmp "http://example.com/source" "destination" 25)
   test "${actual}" = "${expected}"
 }
 
-it_configures_ffmpeg_for_unescaped_25fps_rtsp_to_rtmp() {
-  expected="-fflags nobuffer -flags low_delay -strict experimental -rtsp_transport tcp -r 25 -i http://example.com -c:v copy -c:a aac -ar 44100 -f flv rtmp://localhost/rtsp-25/http%3A%2F%2Fexample.com"
-  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com" 25 skip)
-  test "${actual}" = "${expected}"
-}
-
-it_configures_ffmpeg_for_muxed_unescaped_25fps_rtsp_to_rtmp() {
-  expected="-fflags nobuffer -flags low_delay -strict experimental -rtsp_transport tcp -r 25 -i http://example.com -stream_loop -1 -i http://example.com/sound.mp3 -map 0:v -map 1:a -c:v copy -c:a aac -ar 44100 -shortest -f flv rtmp://localhost/rtsp-25/http%3A%2F%2Fexample.com^http%3A%2F%2Fexample.com%2Fsound.mp3"
-  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com^http%3A%2F%2Fexample.com%2Fsound.mp3" 25 skip)
+it_configures_ffmpeg_for_muxed_25fps_rtsp_to_rtmp() {
+  expected="-use_wallclock_as_timestamps 1 -probesize 32 -fflags nobuffer -flags low_delay -rtsp_transport tcp -r 25 -i http://example.com/source -use_wallclock_as_timestamps 1 -stream_loop -1 -probesize 32 -fflags nobuffer -flags low_delay -i http://example.com/audio.mp3 -map 0:v -map 1:a -movflags +faststart -shortest -fflags +genpts -c:v copy -c:a aac -ac 2 -ar 44100 -ab 128k -f flv rtmp://localhost/rtsp-25/destination"
+  actual=$(rtsp_to_rtmp "http://example.com/source" "destination" 25 "http://example.com/audio.mp3")
   test "${actual}" = "${expected}"
 }
