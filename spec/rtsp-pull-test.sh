113e4db4eaf18d2213a4a29deeffc0a8f5b954bb
Doc Ritezel <doc@ministryofvelocity.com>
2019-02-08 16:31:36 -0800

    Split 30fps and 25fps framerate pulls

--- spec/rtsp-pull-test.sh
+++ spec/rtsp-pull-test.sh
@@ -16,8 +16,20 @@ it_double_encodes_urls() {
   test "${actual}" = "${expected}"
 }
 
-it_configures_ffmpeg_for_rtsp_to_rtmp() {
-  expected="-fflags nobuffer -flags low_delay -rtsp_transport tcp -r 30 -i http://example.com -c copy -shortest -probesize 32 -f flv rtmp://localhost/rtsp/http%253A%252F%252Fexample.com"
-  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com")
+it_configures_ffmpeg_for_25fps_rtsp_to_rtmp() {
+  expected="-fflags nobuffer -flags low_delay -strict experimental -rtsp_transport tcp -r 25 -i http://example.com -c:v copy -c:a aac -ar 44100 -f flv rtmp://localhost/rtsp-25/http%253A%252F%252Fexample.com"
+  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com" 25)
+  test "${actual}" = "${expected}"
+}
+
+it_configures_ffmpeg_for_unescaped_25fps_rtsp_to_rtmp() {
+  expected="-fflags nobuffer -flags low_delay -strict experimental -rtsp_transport tcp -r 25 -i http://example.com -c:v copy -c:a aac -ar 44100 -f flv rtmp://localhost/rtsp-25/http%3A%2F%2Fexample.com"
+  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com" 25 skip)
+  test "${actual}" = "${expected}"
+}
+
+it_configures_ffmpeg_for_muxed_unescaped_25fps_rtsp_to_rtmp() {
+  expected="-fflags nobuffer -flags low_delay -strict experimental -rtsp_transport tcp -r 25 -i http://example.com -stream_loop -1 -i http://example.com/sound.mp3 -map 0:v -map 1:a -c:v copy -c:a aac -ar 44100 -shortest -f flv rtmp://localhost/rtsp-25/http%3A%2F%2Fexample.com^http%3A%2F%2Fexample.com%2Fsound.mp3"
+  actual=$(rtsp_to_rtmp "http%3A%2F%2Fexample.com^http%3A%2F%2Fexample.com%2Fsound.mp3" 25 skip)
   test "${actual}" = "${expected}"
 }
