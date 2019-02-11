f9d9d5d75c6e803cea2fec2ca20c9acdafbaf317
Doc Ritezel <doc@ministryofvelocity.com>
2019-02-10 21:19:43 -0800

    Bump to ffmpeg 4.1

--- spec/rtsp-pull-test.sh
+++ spec/rtsp-pull-test.sh
@@ -17,13 +17,13 @@ it_double_encodes_urls() {
 }
 
 it_configures_ffmpeg_for_25fps_rtsp_to_rtmp() {
-  expected="-use_wallclock_as_timestamps 1 -probesize 32 -fflags nobuffer -flags low_delay -rtsp_transport tcp -r 25 -i http://example.com/source -movflags +faststart -shortest -fflags +genpts -c:v copy -c:a aac -ac 2 -ar 44100 -ab 128k -f flv rtmp://localhost/rtsp-25/destination"
+  expected="-fflags nobuffer -fflags +genpts -fflags +igndts -flags low_delay -rtsp_transport tcp -r 25 -i http://example.com/source -movflags +faststart -af aresample=resampler=soxr -c:v copy -c:a libfdk_aac -ac 2 -ar 44100 -ab 128k -f flv rtmp://localhost/rtsp-25/destination"
   actual=$(rtsp_to_rtmp "http://example.com/source" "destination" 25)
   test "${actual}" = "${expected}"
 }
 
 it_configures_ffmpeg_for_muxed_25fps_rtsp_to_rtmp() {
-  expected="-use_wallclock_as_timestamps 1 -probesize 32 -fflags nobuffer -flags low_delay -rtsp_transport tcp -r 25 -i http://example.com/source -use_wallclock_as_timestamps 1 -stream_loop -1 -probesize 32 -fflags nobuffer -flags low_delay -i http://example.com/audio.mp3 -map 0:v -map 1:a -movflags +faststart -shortest -fflags +genpts -c:v copy -c:a aac -ac 2 -ar 44100 -ab 128k -f flv rtmp://localhost/rtsp-25/destination"
+  expected="-fflags nobuffer -fflags +genpts -fflags +igndts -flags low_delay -rtsp_transport tcp -r 25 -i http://example.com/source -fflags +igndts -fflags +genpts -stream_loop -1 -i http://example.com/audio.mp3 -map 0:v -map 1:a -movflags +faststart -af aresample=resampler=soxr -c:v copy -c:a libfdk_aac -ac 2 -ar 44100 -ab 128k -f flv rtmp://localhost/rtsp-25/destination"
   actual=$(rtsp_to_rtmp "http://example.com/source" "destination" 25 "http://example.com/audio.mp3")
   test "${actual}" = "${expected}"
 }
